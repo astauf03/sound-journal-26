aaaa2079962106b897a70336aa01b100968c9219
astauf03 <astauf03@upenn.edu>
2026-01-19 15:54:48 -0500

    fix base location

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astauf03.github.io/sound-journal-26",
+  site: "https://astauf03.github.io",
+  base: "/sound-journal-26",
   integrations: [preact()],
 
   vite: {
