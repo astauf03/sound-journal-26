d50126e08aa1e01a8a7a0b62c10b3905a2378d27
astauf03 <astauf03@upenn.edu>
2026-01-19 11:21:22 -0500

    first website w/ astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://superb-cascaron-2d453b.netlify.app/"
+  site: "https://superb-cascaron-2d453b.netlify.app/",
+  integrations: [preact()]
 });
